fd5bc8e662b65d400f2a6f9b907b61ca3319797f
diyorbek <mtd.coder@gmail.com>
2023-03-20 19:16:31 +0500

    commit

---
+++ 0052_LengthofLastWord/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        #	return len(s.split()[-1])
+        c = 0
+        for i in s[::-1]:
+            if i == " ":
+                if c >= 1:
+                    return c
+            else:
+                c += 1
+        return c
