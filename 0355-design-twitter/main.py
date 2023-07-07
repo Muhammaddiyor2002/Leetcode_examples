6db0a820ea26bda75705d8584398d155a4897fa0
diyorbek <mtd.coder@gmail.com>
2023-07-07 10:51:57 +0500

    commit

--- 0355-design-twitter/main.py
+++ 0355-design-twitter/main.py
@@ -1,35 +1,55 @@
+import time
+
 class Twitter:
+
     def __init__(self):
-        self.count = 0
-        self.tweetMap = defaultdict(list)  # userId -> list of [count, tweetIds]
-        self.followMap = defaultdict(set)  # userId -> set of followeeId
+        self.tweets = {}  # Foydalanuvchi tweetlarini saqlash uchun lug'at
+        self.followees = {}  # Foydalanuvchilar tomonidan kuzatiladigan odamlarni saqlash uchun lug'at
+        self.time = 0  # Vaqt belgilari
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.tweetMap[userId].append([self.count, tweetId])
-        self.count -= 1
+        # Foydalanuvchi tweetlarini qo'shish uchun
+        if userId not in self.tweets:
+            self.tweets[userId] = []
+        self.tweets[userId].append((tweetId, self.time))
+        self.time += 1
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        res = []
-        minHeap = []
-
-        self.followMap[userId].add(userId)
-        for followeeId in self.followMap[userId]:
-            if followeeId in self.tweetMap:
-                index = len(self.tweetMap[followeeId]) - 1
-                count, tweetId = self.tweetMap[followeeId][index]
-                heapq.heappush(minHeap, [count, tweetId, followeeId, index - 1])
-
-        while minHeap and len(res) < 10:
-            count, tweetId, followeeId, index = heapq.heappop(minHeap)
-            res.append(tweetId)
-            if index >= 0:
-                count, tweetId = self.tweetMap[followeeId][index]
-                heapq.heappush(minHeap, [count, tweetId, followeeId, index - 1])
-        return res
+        # Foydalanuvchining o'zining va kuzatuvchi foydalanuvchilarining tweetlarini qaytarish uchun
+        tweets = []
+        if userId in self.tweets:
+            tweets.extend(self.tweets[userId])
+        if userId in self.followees:
+            for followeeId in self.followees[userId]:
+                if followeeId in self.tweets:
+                    tweets.extend(self.tweets[followeeId])
+        # Vaqt belgilari bo'yicha oxirgi 10 ta tweetni qaytarish uchun
+        tweets.sort(key=lambda x: x[1], reverse=True)
+        return [tweet[0] for tweet in tweets[:10]]
 
     def follow(self, followerId: int, followeeId: int) -> None:
-        self.followMap[followerId].add(followeeId)
+        # Foydalanuvchi boshqa foydalanuvchini kuzatish uchun
+        if followerId not in self.followees:
+            self.followees[followerId] = set()
+        self.followees[followerId].add(followeeId)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
-        if followeeId in self.followMap[followerId]:
-            self.followMap[followerId].remove(followeeId)
+        # Foydalanuvchi kuzatuvchi foydalanuvchini kuzatishdan chiqarish uchun
+        if followerId in self.followees and followeeId in self.followees[followerId]:
+            self.followees[followerId].remove(followeeId)
+
+
+# Namuna ishlatish
+
+twitter = Twitter()
+
+# Foydalanuvchilar
+twitter.postTweet(1, 5)  # Foydalanuvchi 1, tweet 5 ni joyladi
+print(twitter.getNewsFeed(1))  # Foydalanuvchi 1 ning tweetlarini ko'rish (faqat o'zining tweetlari)
+
+twitter.follow(1, 2)  # Foydalanuvchi 1, foydalanuvchi 2 ni kuzatishni boshladi
+twitter.postTweet(2, 6)  # Foydalanuvchi 2, tweet 6 ni joyladi
+print(twitter.getNewsFeed(1))  # Foydalanuvchi 1 ning tweetlarini ko'rish (o'zining tweetlari + kuzatgan foydalanuvchi tweeti)
+
+twitter.unfollow(1, 2)  # Foydalanuvchi 1, foydalanuvchi 2 ni kuzatishdan chiqdi
+print(twitter.getNewsFeed(1))  # Foydalanuvchi 1 ning tweetlarini ko'rish (faqat o'zining tweetlari)
