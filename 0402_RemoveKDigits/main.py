01834c99ad2fdb46ef6478b5bcc1094098a4482c
diyorbek <mtd.coder@gmail.com>
2023-07-30 00:12:20 +0500

    commit

---
+++ 0402_RemoveKDigits/main.py
@@ -0,0 +1,21 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        if k >= len(num):
+            return "0"
+
+        stack = []
+        for digit in num:
+            while k > 0 and stack and stack[-1] > digit:
+                stack.pop()
+                k -= 1
+            stack.append(digit)
+
+        # Handle the case when there are still k digits to be removed
+        while k > 0:
+            stack.pop()
+            k -= 1
+
+        # Remove leading zeros
+        result = ''.join(stack).lstrip('0')
+
+        return result if result else "0"
