ade92567e0e61a304f0dcfe3bd33bbc85d4821ea
diyorbek <mtd.coder@gmail.com>
2024-03-23 01:15:12 +0500

    commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Matritsaning elementlarini tanlash
+matritsa = [[3, 5]]
+
+# Indekslarni i va j ga ajratib olish
+i, j = 0, 0
+
+# Matritsaning yig'indisini hisoblash
+yigindisi = matritsa[i][j] + matritsa[i][j+1]
+
+# Natijani ekranga chiqarish
+print("Matritsaning yig'indisi:", yigindisi)
